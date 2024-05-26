1e0055d0234c068a6cc785a16e878ccedf9030d2
spowers0409 <spowers0409@gmail.com>
2024-05-26 00:57:42 -0500

    add github link using JS

--- script.js
+++ script.js
@@ -76,3 +76,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
         .catch(error => console.error('Error', error));
     });
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.createElement('button');
+    button.classList.add('github-button');
+
+    const img = document.createElement('img');
+    img.src = 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png';
+    img.alt = 'Github';
+
+    button.appendChild(img);
+
+    button.addEventListener('click', () => {
+        window.location.href = 'https://github.com/spowers0409/JS_ajax';
+    });
+    document.body.appendChild(button)
+});
